9ade73365cf318f71c589a0fb8b0f19f3bff800b
Mutlaq The Great <mutlaqalsadeed@gmail.com>
2024-06-12 22:47:24 +0300

    final veresion

--- index.js
+++ index.js
@@ -1,12 +1,184 @@
+/** 
+// ! Task-002 (1)
+ 
+Question:- 
+
+// * Create a password validator function.
+// * You will receive a parameter called "password"
+// * Password must be of type string
+// * Password must be more than 8 characters
+// * Password must be less than 30 characters
+// * Password must include one uppercase letter
+// * Password must include one lowercase letter
+// * Password must include one special letter
+// * Password must include one number
+// * Function must return the missing validation point for example the password "mutlaq12345" does not have an uppercase letter or a special character as a string.
+// * Use the following arrays
+ 
+
+
+  const stringNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const specialCharacters = [
+  '.',
+  '*',
+  '/',
+  '-',
+  ';',
+  '{',
+  '}',
+  '[',
+  ']',
+  '?',
+  '\\',
+  '+',
+  ':',
+  '`',
+  '~',
+  '_',
+  '=',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '',
+  '|',
+  ',',
+  ' ',
+];
+ const lowercaseLetters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+ const uppercaseLetters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+Solution: -
+ 
+const passwordErrors = {
+	required: 'Password Must be Provided',
+	type: 'Password must be of type string',
+	min: 'Password must be more than 8 characters',
+	max: 'Password must be less than 30 characters',
+	includeUpperCaseChar: 'Password must include at least one uppercase letter',
+	includeLowerCaseChar: 'Password must include at least one lowercase letter',
+	includeSpecialChar: 'Password must include at least one special letter',
+	includeNumbers: 'Password must include at least one number',
+};
+ function passwordValidator(password) {
+		const validationErrors = [];
+		if (!password) validationErrors.push(passwordErrors.required);
+
+		const passwordLength = password.length;
+
+		if (!(typeof password == 'string')) validationErrors.push(passwordErrors.type);
+
+		if (passwordLength <= 8) validationErrors.push(passwordErrors.min);
+
+		if (passwordLength >= 30) validationErrors.push(passwordErrors.max);
+
+		if (!hasCharacterFromSet(password, uppercaseLetters))
+			validationErrors.push(passwordErrors.includeUpperCaseChar);
+
+		if (!hasCharacterFromSet(password, lowercaseLetters))
+			validationErrors.push(passwordErrors.includeLowerCaseChar);
+
+		if (!hasCharacterFromSet(password, stringNumbers))
+			validationErrors.push(passwordErrors.includeNumbers);
+
+		if (!hasCharacterFromSet(password, specialCharacters))
+			validationErrors.push(passwordErrors.includeSpecialChar);
+
+		if (validationErrors.length > 0)
+			return validationErrors.map((error) => error + '\n').join('');
+
+		return 'Password is Valid';
+ }
+
+ function hasCharacterFromSet(password = '', characterSet) {
+		return characterSet.some((char) => password.includes(char) && !!char);
+ }
+
+ console.log(passwordValidator('m'));
+ console.log(passwordValidator('mU'));
+ console.log(passwordValidator('mU1'));
+ console.log(passwordValidator('mU1@ssssss'));
+
+Question: -
+// * Turn the previous function into a "function expression" and turn it to an arrow function
+*/
+
 /**
  ### Create a function that takes a number as an argument.
  ### Add up all the numbers from 1 to the number you passed to the function.
  ### For example, if the input is 4 then your function should return 10
  ### because 1 + 2 + 3 + 4 = 10.
  */
-function addUp(num) {
+function addUp(number) {
 	let sum = 0;
-	for (let i = 0; i <= num; i++) {
+	for (let i = 0; i <= number; i++) {
+		sum += i;
+	}
+	return sum;
+}
+function addUp(number) {
+	let sum = 0;
+	for (let i = 1; i <= number; i++) {
 		sum += i;
 	}
 	return sum;
@@ -17,9 +189,14 @@ function addUp(num) {
  # Check if all elements are numbers using [every](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every)
  */
 function getFirstValue(arr) {
-	if (arr.every((element) => typeof element === 'number')) {
-		return arr[0];
-	}
+	const isArrayFalsy = !arr; // null or undefined
+	if (isArrayFalsy) throw new Error('An Array must be provided');
+
+	const isParamOfTypeArray = Array.isArray(arr);
+	if (!isParamOfTypeArray) throw new Error('Provided parameter was not an array');
+
+	const isEveryValueANumber = arr.every((element) => typeof element === 'number');
+	if (isEveryValueANumber) return arr[0];
 }
 
 /**
@@ -32,18 +209,24 @@ function getFirstValue(arr) {
  */
 
 function minMax(arr) {
-	const result = [];
-	result.push(
-		arr.reduce((previousValue, current) =>
-			previousValue < current ? previousValue : current
-		)
-	);
+	const isArrayFalsy = !arr; // null or undefined
+	if (isArrayFalsy) throw new Error('An Array must be provided');
 
-	result.push(
-		arr.reduce((previousValue, current) =>
-			previousValue > current ? previousValue : current
-		)
+	const isParamOfTypeArray = Array.isArray(arr);
+	if (!isParamOfTypeArray) throw new Error('Provided parameter was not an array');
+
+	const isEveryValueANumber = arr.every((element) => typeof element === 'number');
+	if (!isEveryValueANumber) throw new Error('You must provide an array of numbers');
+
+	const result = [];
+	const min = arr.reduce((previousValue, current) =>
+		previousValue < current ? previousValue : current
 	);
+	result.push(min);
+	const max = arr.reduce((previousValue, current) =>
+		previousValue > current ? previousValue : current
+	);
+	result.push(max);
 
 	return result;
 }
@@ -142,4 +325,105 @@ function getTotalPrice(groceries) {
 		(total, obj) => +(total + obj.quantity * obj.price).toFixed(1),
 		0
 	);
+}
+
+/**
+### Create a function that takes a string as a parameter, this function should return the first reappearance of a 
+char that appeared at least twice in an object that looks like this
+```js
+let str = "ABCATRRF"
+{
+  'A': [0, 3]
+  // 0 is the index the first time A appeared, 3 is the second time 
+}
+``` 
+### The function should return the char that completes a cycle first meaning in this string **YXZXYTUVXWV**
+### Y appeared in index 0 and index 4, but the function should return
+```js
+let str = "YXZXYTUVXWV"
+{
+  'X': [1, 3]
+  // beacause the letter X finished faster than Y even though Y appeared first
+}
+```
+### The function should return an **__empty object**__ if the passed argument is either null, undefined, empty string, or no recurring item exists.
+```js
+function recurIndex(str) {
+    
+}
+// TEST CASES
+recurIndex("DXTDXTXDTXD") ➞ {"D": [0, 3]}
+// D first appeared at index 0, resurfaced at index 3
+// T appeared and resurfaced at indices 3 and 6 but D completed the cycle first
+
+recurIndex("YXZXYTUVXWV") ➞ {"X": [1, 3]}
+
+recurIndex("YZTTZMNERXE") ➞ {"T": [2, 3]}
+
+recurIndex("AREDCBSDERD") ➞ {"D": [3, 7]}
+
+recurIndex("KDXTDATTDDX") ➞ {"D":[1,4]}
+
+recurIndex("AKEDCBERSDA") ➞ {"E":[2,6]}
+
+recurIndex("DXKETRETXDK") ➞ {"E":[3,6]}
+
+recurIndex("") ➞ {}
+
+recurIndex(null) ➞ {}
+```
+ */
+
+function recurIndex(str) {
+	const object = {};
+	let firstOccurrenceIndex;
+	let secondOccurrenceIndex;
+	let key;
+	if (!str) return {};
+	str.split('').find((firstChar, firstIndex, strArr) =>
+		strArr.find((secondChar, secondIndex) => {
+			if (firstChar === secondChar && firstIndex !== secondIndex && !object[firstChar]) {
+				firstOccurrenceIndex = firstIndex;
+				secondOccurrenceIndex = secondIndex;
+				key = firstChar;
+				object[key] = [firstOccurrenceIndex, secondOccurrenceIndex];
+			}
+		})
+	);
+	if (!key) return {};
+	let min = Number.MAX_SAFE_INTEGER;
+	let minKey;
+	for (const property in object) {
+		if (object[property][1] - object[property][0] < min) {
+			min = object[property][1] - object[property][0];
+			minKey = property;
+		}
+	}
+	return {[minKey]: object[minKey]};
+}
+const recurIndex = (str) => {
+	for (const [i, char] of [...(str || '')].entries()) {
+		const j = str.indexOf(char);
+		if (j < i) return {[char]: [j, i]};
+	}
+	return {};
+};
+
+function recurIndex(str) {
+	if (!str) {
+		return {};
+	}
+	let characters = {},
+		indices = [],
+		character = '';
+	for (i = 0; i < str.length; i++) {
+		if (Object.keys(characters).includes(str[i])) {
+			character = str[i];
+			indices.push(characters[str[i]], i);
+			break;
+		} else {
+			characters[str[i]] = i;
+		}
+	}
+	return indices.length ? {[character]: indices} : {};
 }
